848125b3a683521b0c0d3e6c1f6f4a6253ee12ab
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-12-10 10:12:59 +0200

    fix header colors and logo validation error

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -26,9 +26,7 @@ const Header = () => {
   return (
     <header
       className={`${
-        scrolling
-          ? "bg-[#173D33] bg-opacity-50 backdrop-blur-[2px]"
-          : ""
+        scrolling ? "bg-[#fff]  backdrop-blur-[2px]" : ""
       } fixed left-0 right-0 mx-auto py-[10px] transition-colors duration-200 z-[10]`}
     >
       <div className="custom-container flex">
